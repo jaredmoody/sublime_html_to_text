1f1e3b09e90463b2f8ffade7cfa4661205b3f388
jaredmoody <jared@jaredmoody.com>
2014-05-06 00:27:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# HTML to Text plugin for Sublime Text
+
+Converts files, selection and clipboard content from HTML to Text/Valid markdown
+using html2text
+
+## Installation
+
+### Git installation
+
+Clone the repository in your Sublime Text "Packages" directory:
+
+    git clone https://github.com/jaredmoody/sublime-html-to-text.git "HTML2Text"
+
+The "Packages" directory is located at:
+
+* OS X:
+
+        ~/Library/Application Support/Sublime Text 3/Packages/
+
+* Linux:
+
+        ~/.config/sublime-text-3/Packages/
+
+* Windows:
+
+        %APPDATA%/Sublime Text 3/Packages/
+
+## Usage
+
+### In Command Palette:
+
+* **HTML2Text: Convert file**
+* **HTML2Text: Convert selection**
+* **HTML2Text: Convert clipboard content**
+
+## Credits
+
+Thanks to
+[sublime-html-to-haml](https://github.com/pavelpachkovskij/sublime-html-to-haml)
+for a pattern to follow and to [html2text](https://github.com/aaronsw/html2text)
+for all the heavy lifting :)
---
+++ html2text_plugin.py
@@ -0,0 +1,42 @@
+import json
+import sublime, sublime_plugin
+import html2text
+
+class HtmlToTextFromFileCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    source = self.view.file_name()
+    target = source + '.md'
+    with open(source, 'r') as f:
+      html = f.read()
+    text = html2text.html2text(html)
+    if text != None:
+      with open(target, 'w') as f:
+        f.write(text)
+      self.view.window().open_file(target)
+
+  def is_enabled(self):
+    return True
+
+class HtmlToTextFromSelectionCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    for region in self.view.sel():
+      if not region.empty():
+        html = self.view.substr(region)
+        text = html2text.html2text(html)
+        if text != None:
+          self.view.replace(edit, region, text)
+
+  def is_enabled(self):
+    return True
+
+class HtmlToTextFromClipboardCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    html = sublime.get_clipboard()
+    text = html2text.html2text(html)
+    if text != None:
+      for region in self.view.sel():
+        self.view.replace(edit, region, text)
+
+  def is_enabled(self):
+    return True
+
